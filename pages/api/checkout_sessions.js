9e9e106554c0a5782e26e502a4c3f1034739e504
rony <you@example.com>
2024-03-12 23:28:40 +0600

    first commit

---
+++ pages/api/checkout_sessions.js
@@ -0,0 +1,34 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      // Create Checkout Sessions from body params.
+      const amount = 20;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: "Your Product",
+              },
+              unit_amount: Math.round(parseFloat(amount) * 100), // Stripe requires amount in cents
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+        automatic_tax: { enabled: true },
+      });
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,150 @@
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+export default function PreviewPage() {
+  React.useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
+    }
+  }, []);
+
+  return (
+    <form action="/api/checkout_sessions" method="POST">
+      <section>
+        <div className="product">
+          <img
+            src="https://i.imgur.com/EHyR2nP.png"
+            alt="The cover of Stubborn Attachments"
+          />
+          <div class="description">
+            <h3>Stubborn Attachments</h3>
+            <h5>$20.00</h5>
+          </div>
+        </div>
+        <button type="submit" role="link">
+          Checkout
+        </button>
+      </section>
+      <style jsx>
+        {`
+          section {
+            background: #ffffff;
+            display: flex;
+            flex-direction: column;
+            width: 400px;
+            height: 112px;
+            border-radius: 6px;
+            justify-content: space-between;
+          }
+          button {
+            height: 36px;
+            background: #556cd6;
+            border-radius: 4px;
+            color: white;
+            border: 0;
+            font-weight: 600;
+            cursor: pointer;
+            transition: all 0.2s ease;
+            box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+          }
+          button:hover {
+            opacity: 0.8;
+          }
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: #242d60;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+              "Helvetica Neue", "Ubuntu", sans-serif;
+            height: 100vh;
+            margin: 0;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+          section {
+            background: #ffffff;
+            display: flex;
+            flex-direction: column;
+            width: 400px;
+            height: 112px;
+            border-radius: 6px;
+            justify-content: space-between;
+          }
+          .product {
+            display: flex;
+          }
+          .description {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+          }
+          p {
+            font-style: normal;
+            font-weight: 500;
+            font-size: 14px;
+            line-height: 20px;
+            letter-spacing: -0.154px;
+            color: #242d60;
+            height: 100%;
+            width: 100%;
+            padding: 0 20px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            box-sizing: border-box;
+          }
+          img {
+            border-radius: 6px;
+            margin: 10px;
+            width: 54px;
+            height: 57px;
+          }
+          h3,
+          h5 {
+            font-style: normal;
+            font-weight: 500;
+            font-size: 14px;
+            line-height: 20px;
+            letter-spacing: -0.154px;
+            color: #242d60;
+            margin: 0;
+          }
+          h5 {
+            opacity: 0.5;
+          }
+          button {
+            height: 36px;
+            background: #556cd6;
+            color: white;
+            width: 100%;
+            font-size: 14px;
+            border: 0;
+            font-weight: 500;
+            cursor: pointer;
+            letter-spacing: 0.6;
+            border-radius: 0 0 6px 6px;
+            transition: all 0.2s ease;
+            box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+          }
+          button:hover {
+            opacity: 0.8;
+          }
+        `}
+      </style>
+    </form>
+  );
+}
